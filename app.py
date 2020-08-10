3ee64f4618f5b69c278bd5c651eec985d08ec00d
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-10 18:15:48 +0800

    Database Created namely Profile

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    number = db.Column(db.Integer, unique=True)
+
+    def __repr__(self):
+        return "<Entry: %r" % self.number
 
 @app.route('/')
 def index():
