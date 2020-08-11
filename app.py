ad6869a7b2fad3d9ef4c134a8b8c17743363d2bf
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-11 10:27:20 +0800

    Changed to IDE now using Pycharm. Trying to commit.

--- app.py
+++ app.py
@@ -28,14 +28,20 @@ class Profile(db.Model):
         return "<Entry: %r Name: %r" % self.number, self.name
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
     return render_template('loginform.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
