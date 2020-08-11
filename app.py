a9bd6733e48d6037ae3bfe8f56fb6d47056dea60
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-11 14:53:48 +0800

    Flask Login Form

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+
 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profile.db'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember_me')
 
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
+    phone_number = db.Column(db.String(11), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     number = db.Column(db.Integer, unique=True)
 
@@ -33,9 +44,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('loginform.html')
+    form = LoginForm()
+
+
+    return render_template('loginform.html', form=form)
 
 
 @app.route('/register')
