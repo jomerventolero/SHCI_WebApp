7c9b81390cbde6d8d4a342c9a72a6cc4df0c5c39
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-11 22:37:05 +0800

    Fixing Adding User

--- app.py
+++ app.py
@@ -3,26 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-
+from wtforms.validators import InputRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profile.db'
+Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = "thisismysecretkey"
 
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember_me')
+    remember = BooleanField('remember me')
 
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    phone_number = StringField('phone', validators=[InputRequired(), Length(max=11)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    phone_number = db.Column(db.String(11), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    number = db.Column(db.Integer, unique=True)
 
     @property
     def password(self):
@@ -38,6 +40,14 @@ class Profile(db.Model):
     def __repr__(self):
         return "<Entry: %r Name: %r" % self.number, self.name
 
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    phone_number = db.Column(db.String(11), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    number = db.Column(db.Integer, unique=True)
+
 
 @app.route('/')
 def index():
@@ -47,14 +57,22 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    return render_template('login.html', form=form)
 
 
-    return render_template('loginform.html', form=form)
-
-
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    if request.method == 'POST':
+        usr = form.username.data
+        pwd = form.password.data
+        email = form.email.data
+        phone_number = form.phone_number.data
+        new_user = Profile(username=usr, password=pwd, phone_number=phone_number, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
